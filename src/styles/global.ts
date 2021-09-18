4aef95c741772e64cde1dab57a502548a2ea4bdb
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-09-18 18:21:14 -0300

    Creating global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    
+    :root{
+        --red: #E52E4D;
+        --blue: #5429cc;
+        
+        --blue-light: #6933ff;
+        
+        --text-tilte: #363f5f;
+        --text-title: #969cb3;
+        
+        --background: #f0f2f5;
+        --shape: #ffffff;
+    }
+    
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html{
+        @media (max-width: 1080px){
+            font-size: 93.5%;
+        }
+
+        @media (max-width: 720px){
+            font-size: 87.5%;
+        }
+    }
+
+    body{
+        background-color: var(---background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    [disabled]{
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
