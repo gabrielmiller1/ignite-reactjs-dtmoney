f247f64e4cdb954e3a1a1e8b153c437275d609e3
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-09-19 08:53:24 -0300

    Creating Dashboard and Summary components

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from "../Dashboard/styles";
+import { Summary } from "../Summary";
+
+export function Dashboard() {
+    return(
+        <Container>
+        <Summary />
+        </Container>
+    );
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     :root{
         --red: #E52E4D;
         --blue: #5429cc;
+        --green: #33cc95;
         
         --blue-light: #6933ff;
         
